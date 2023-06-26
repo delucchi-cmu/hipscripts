9babc13b69277846698760a2851dd91ae1232c29
Melissa DeLucchi <mmd11@epyc.astro.washington.edu>
2023-06-26 06:52:05 -0700

    checkpoint

---
+++ epyc/allwise/README.md
@@ -0,0 +1,22 @@
+## Running full allwise
+
+This is from a resumed run, so I lost the mapping/splitting lines, 
+and like 20k of the reducing lines too.
+
+Binning  : 100%|██████████| 1/1 [00:14<00:00, 15.00s/it]
+Reducing : 100%|██████████| 830/830 [27:40<00:00,  2.00s/it]
+Finishing: 100%|██████████| 6/6 [00:39<00:00,  6.52s/it]
+
+## Running neowise stats only
+
+real    3524m16.522s
+user    34283m20.342s
+sys     2090m15.450s
+
+Mapping  : 100%|██████████| 72/72 [58:38:35<00:00, 2932.16s/it]
+Binning  : 100%|██████████| 1/1 [05:26<00:00, 326.84s/it]
+Finishing: 100%|██████████| 6/6 [00:01<00:00,  3.21it/s]
+
+## Running full neowise
+
+TODO
--- epyc/allwise/run_neowise.py
+++ epyc/allwise/run_neowise.py
@@ -7,11 +7,10 @@ from hipscat_import.catalog.arguments import ImportArguments
 from hipscat_import.catalog.file_readers import CsvReader
 
 if __name__ == "__main__":
-    type_frame = pd.read_csv("types.csv")
+    type_frame = pd.read_csv("types.csv", keep_default_na=False)
     type_map = dict(zip(type_frame["name"], type_frame["type"]))
     args = ImportArguments(
-        output_catalog_name="neowise_test",
-        # input_file_list=["/epyc/data3/NEOWISE-yr8/irsa.ipac.caltech.edu/data/download/neowiser_year8/neowiser.neowiser_yr8_p1bs_psd.LOAD_2071.csv.bz2"],
+        output_catalog_name="neowise_yr8",
         input_path="/epyc/data3/NEOWISE-yr8/irsa.ipac.caltech.edu/data/download/neowiser_year8/",
         input_format="csv.bz2",
         file_reader=CsvReader(
@@ -26,12 +25,11 @@ if __name__ == "__main__":
         id_column="SOURCE_ID",
         pixel_threshold=2_000_000,
         tmp_dir="/data3/epyc/data3/hipscat/tmp/",
-        debug_stats_only=True,
-        overwrite=True,
-        highest_healpix_order=10,
+        highest_healpix_order=9,
         dask_n_workers=10,
         dask_threads_per_worker=1,
         dask_tmp="/data3/epyc/data3/hipscat/tmp/",
         output_path="/data3/epyc/data3/hipscat/catalogs/",
+        completion_email_address="delucchi@andrew.cmu.edu",
     )
     runner.pipeline(args)
