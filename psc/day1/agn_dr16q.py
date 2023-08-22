ea91e07c757e2be26d528f1bf06af1ae426cb48d
delucchi-cmu <delucchi@andrew.cmu.edu>
2023-08-22 02:06:29 -0400

    psc checkpoint

---
+++ psc/day1/agn_dr16q.py
@@ -0,0 +1,51 @@
+import glob
+import re
+
+import pandas as pd
+from astropy.table import Table
+from astropy.table.table import descr
+from tqdm import tqdm
+
+
+def convert():
+    base_dir = "/home/delucchi/td_data/agns/"
+
+    table = Table.read(
+        os.path.join(base_dir, "dr16q_prop_May16_2023.fits.gz"), memmap=True
+    )
+
+    total_rows = len(table)
+    print("total_rows", total_rows)
+    read_rows = 0
+    chunksize = 100_000
+    chunk_num = 0
+
+    while read_rows < total_rows:
+        subtable = table[read_rows : read_rows + chunksize]
+        read_rows += chunksize
+
+        new_table = Table()
+        for col in subtable.columns.values():
+            descriptor = descr(col)
+            col_name = descriptor[0]
+            col_shape = descriptor[2]
+            if col_shape == ():
+                # print("scalar:", col_name)
+                new_table.add_column(col)
+            else:
+                # print("multi-column", col_name)
+                data_t = col.data.T
+                for index in range(col_shape[0]):
+                    arr_name = f"{col_name}_arr{index}"
+                    # print("    ", arr_name)
+                    new_table.add_column(data_t[index], name=arr_name)
+
+        new_table.to_pandas().to_parquet(
+            os.path.join(base_dir, f"dr16q_prop_May16_2023_{chunk_num}.parquet")
+        )
+        print("wrote chunk", chunk_num)
+        chunk_num += 1
+
+
+if __name__ == "__main__":
+    convert()
--- psc/day1/sdss_1file.py
+++ psc/day1/sdss_1file.py
@@ -1,17 +1,58 @@
 import glob
+import os
 import re
+
+import hipscat_import.pipeline as runner
 import pandas as pd
-from tqdm import tqdm
+import pyarrow as pa
+import pyarrow.parquet as pq
 from astropy.table import Table
 from astropy.table.table import descr
+from dask.distributed import Client
+from hipscat_import.catalog.arguments import ImportArguments
+from tqdm import tqdm
+
+
+def do_import():
+    local_tmp = os.path.expandvars("$LOCAL")
+    args = ImportArguments(
+        output_catalog_name="sdss_1file",
+        input_path="/ocean/projects/phy210048p/shared/hipscat/raw/sdss_parquet/",
+        input_format="parquet",
+        ra_column="RA",
+        dec_column="DEC",
+        id_column="ID",
+        pixel_threshold=1_000_000,
+        tmp_dir=local_tmp,
+        overwrite=True,
+        resume=True,
+        highest_healpix_order=7,
+        dask_n_workers=1,
+        dask_threads_per_worker=1,
+        dask_tmp=local_tmp,
+        debug_stats_only=True,
+        completion_email_address="delucchi@andrew.cmu.edu",
+        output_path="/ocean/projects/phy210048p/shared/hipscat/test_catalogs/",
+    )
+
+    with Client(
+        local_directory=args.dask_tmp,
+        n_workers=args.dask_n_workers,
+        threads_per_worker=args.dask_threads_per_worker,
+        memory_limit=None,
+    ) as client:
+        runner.pipeline_with_client(args, client)
+
 
 def convert():
-    files = glob.glob("/ocean/projects/phy210048p/shared/hipscat/raw/sdss/calibObj-000094**.fits.gz")
+    files = glob.glob(
+        "/ocean/projects/phy210048p/shared/hipscat/raw/sdss/calibObj-000094**.fits.gz"
+    )
     files.sort()
 
-    for in_file in tqdm(files, bar_format='{l_bar}{bar:80}{r_bar}'):
+    for in_file in tqdm(files, bar_format="{l_bar}{bar:80}{r_bar}"):
 
-        match = re.match(r".*(calibObj-.*-star).fits.gz", str(file))
+        match = re.match(r".*(calibObj-.*-star).fits.gz", str(in_file))
         file_prefix = match.group(1)
         out_file = f"/ocean/projects/phy210048p/shared/hipscat/raw/sdss_parquet/{file_prefix}.parquet"
 
@@ -24,13 +65,15 @@ def convert():
             col_shape = descriptor[2]
             if col_shape == (5,):
                 data_t = col.data.T
-                for index, band_char in enumerate('ugriz'):
+                for index, band_char in enumerate("ugriz"):
                     new_table.add_column(data_t[index], name=f"{col_name}_{band_char}")
             elif col_shape == ():
                 new_table.add_column(col)
 
         new_table.to_pandas().to_parquet(out_file)
 
+
 if __name__ == "__main__":
-    convert()
+    # convert()
+    do_import()
     # read_fits()
