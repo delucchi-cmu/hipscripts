7dea1b71d61f6186dfc8a60edc88ddef3f56aae1
Melissa DeLucchi <mmd11@epyc.astro.washington.edu>
2024-01-17 08:29:28 -0800

    ray checkpoint

--- epyc/fast_selection/agn_index.py
+++ epyc/fast_selection/agn_index.py
@@ -1,20 +1,34 @@
 import hipscat_import.pipeline as runner
 from hipscat_import.index.arguments import IndexArguments
 
+import ray
+from ray.util.dask import enable_dask_on_ray, disable_dask_on_ray
+from dask.distributed import Client
+
 def create_index():
+    context = ray.init(
+        num_cpus=12,
+        _temp_dir="/data3/epyc/projects3/mmd11_hipscat/ray_spill",
+    )
+
+    enable_dask_on_ray()
     args = IndexArguments(
         input_catalog_path="/data3/epyc/data3/hipscat/catalogs/dr16q_constant",
         indexing_column="SDSS_NAME",
         output_path="/data3/epyc/data3/hipscat/test_catalogs",
-        output_artifact_name="agn_sdss_id",
-        # include_hipscat_index=False,
+        output_artifact_name="agn_sdss_id2",
+        include_hipscat_index=False,
         compute_partition_size=20_000_000,
-        dask_n_workers=5,
-        dask_threads_per_worker=1,
-        dask_tmp="/data3/epyc/data3/hipscat/tmp/",
         completion_email_address="delucchi@andrew.cmu.edu",
     )
-    runner.pipeline(args)
+    with Client(
+        local_directory="/data3/epyc/projects3/mmd11_hipscat/ray_spill",
+        n_workers=12,
+        threads_per_worker=1,
+    ) as client:
+        runner.pipeline_with_client(args, client)
+
+    disable_dask_on_ray()
 
 if __name__ == "__main__":
     create_index()
--- epyc/fast_selection/ztfdr14_index.py
+++ epyc/fast_selection/ztfdr14_index.py
@@ -1,20 +1,37 @@
 import hipscat_import.pipeline as runner
 from hipscat_import.index.arguments import IndexArguments
 
+import ray
+from ray.util.dask import enable_dask_on_ray, disable_dask_on_ray
+from dask.distributed import Client
+
 def create_index():
+
+    context = ray.init(
+        num_cpus=12,
+        _temp_dir="/data3/epyc/projects3/mmd11_hipscat/ray_spill",
+    )
+
+    enable_dask_on_ray()
     args = IndexArguments(
         input_catalog_path="/data3/epyc/data3/hipscat/catalogs/ztf_axs/ztf_dr14",
         indexing_column="ps1_objid",
         output_path="/data3/epyc/data3/hipscat/test_catalogs",
         output_artifact_name="ztf_id",
-        # include_hipscat_index=False,
+        include_hipscat_index=False,
         compute_partition_size=20_000_000,
-        dask_n_workers=5,
-        dask_threads_per_worker=1,
-        dask_tmp="/data3/epyc/data3/hipscat/tmp/",
         completion_email_address="delucchi@andrew.cmu.edu",
+        overwrite=True,
     )
-    runner.pipeline(args)
+
+    with Client(
+        local_directory="/data3/epyc/projects3/mmd11_hipscat/ray_spill",
+        n_workers=12,
+        threads_per_worker=1,
+    ) as client:
+        runner.pipeline_with_client(args, client)
+
+    disable_dask_on_ray()
 
 if __name__ == "__main__":
     create_index()
