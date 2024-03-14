5422571e4e4ad933de68dc2628f2721414707a6d
Melissa DeLucchi <113376043+delucchi-cmu@users.noreply.github.com>
2024-03-14 08:40:31 -0400

    formatting

--- .github/workflows/compare_versions.sh
+++ .github/workflows/compare_versions.sh
@@ -5,18 +5,18 @@ do
     echo "Finding version numbers for $package_name"
 
     conda_version=$(conda search -c conda-forge $package_name | tail -n1 | awk '{print $2}')
-    echo "conda version=$conda_version"
+    echo "  * conda version=$conda_version"
 
     pypi_version=$(get_pypi_latest_version $package_name)
-    echo "pypi version=$pypi_version"
+    echo "  * pypi version=$pypi_version"
 
-    if ["$conda_version" != "$pypi_version"]
+    if [$conda_version != $pypi_version]
     then 
         some_failure=true
     fi
 done
 
-if ["$some_failure" == true]
+if [$some_failure == true]
 then
     exit 1
 fi
