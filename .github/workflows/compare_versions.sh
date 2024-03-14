9f30f7cfea83e96f3d448ec42380b8f980a67520
Melissa DeLucchi <113376043+delucchi-cmu@users.noreply.github.com>
2024-03-14 08:34:15 -0400

    all in bash

--- .github/workflows/compare_versions.sh
+++ .github/workflows/compare_versions.sh
@@ -1,12 +1,22 @@
-echo "Finding version numbers for $1"
+some_failure=false
 
-conda_version=$(conda search -c conda-forge $1 | tail -n1 | awk '{print $2}')
-echo "conda version=$conda_version"
+for i in "hipscat" "hipscat-import" "lsdb"
+do
+    echo "Finding version numbers for $i"
 
-pypi_version=$(get_pypi_latest_version $1)
-echo "pypi version=$pypi_version"
+    conda_version=$(conda search -c conda-forge $1 | tail -n1 | awk '{print $2}')
+    echo "conda version=$conda_version"
 
-if ["$conda_version" != "$pypi_version"]
-then 
+    pypi_version=$(get_pypi_latest_version $1)
+    echo "pypi version=$pypi_version"
+
+    if ["$conda_version" != "$pypi_version"]
+    then 
+        some_failure=true
+    fi
+done
+
+if ["$some_failure" == true]
+then
     exit 1
 fi
