5cb6a4546b1289840f62db96c72ea54d5ce50d52
Melissa DeLucchi <113376043+delucchi-cmu@users.noreply.github.com>
2024-03-14 08:20:55 -0400

    oops. don't read.

--- .github/workflows/compare_versions.sh
+++ .github/workflows/compare_versions.sh
@@ -1,11 +1,9 @@
-read package_name
+echo "Finding version numbers for $1"
 
-echo "Finding version numbers for $package_name"
-
-conda_version=$(conda search -c conda-forge $package_name | tail -n1 | awk '{print $2}')
+conda_version=$(conda search -c conda-forge $1 | tail -n1 | awk '{print $2}')
 echo "conda version=$conda_version"
 
-pypi_version=$(get_pypi_latest_version $package_name)
+pypi_version=$(get_pypi_latest_version $1)
 echo "pypi version=$pypi_version"
 
 if ["$conda_version" != "$pypi_version"]
