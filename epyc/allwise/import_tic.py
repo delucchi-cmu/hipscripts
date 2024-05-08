93ebf5cac8c8075cd4093e223820b3487ce056d4
Melissa DeLucchi <mmd11@epyc.astro.washington.edu>
2024-05-08 12:40:01 -0700

    checkpoint new tic script.

--- epyc/allwise/import_tic.py
+++ epyc/allwise/import_tic.py
@@ -1,3 +1,5 @@
+import glob
+
 import hipscat_import.pipeline as runner
 import pandas as pd
 from hipscat_import.catalog.arguments import ImportArguments
@@ -6,10 +8,12 @@ from hipscat_import.catalog.file_readers import CsvReader
 if __name__ == "__main__":
     type_frame = pd.read_csv("tic_types.csv")
     type_map = dict(zip(type_frame["name"], type_frame["type"]))
+
+    files = glob.glob("/data3/epyc/data3/hipscat/raw/tic_csv/*.csv.gz")
+    files.sort()
     args = ImportArguments(
-        output_catalog_name="tic_1",
-        input_path="/data3/epyc/data3/hipscat/raw/tic_csv/",
-        input_format="csv.gz",
+        output_artifact_name="tic_1",
+        input_file_list=files,
         file_reader=CsvReader(
             header=None,
             column_names=type_frame["name"].values.tolist(),
@@ -18,16 +22,14 @@ if __name__ == "__main__":
         ),
         ra_column="ra",
         dec_column="dec",
-        id_column="ID",
         pixel_threshold=1_000_000,
         tmp_dir="/data3/epyc/data3/hipscat/tmp/",
-        overwrite=True,
-        resume=True,
+        resume=False,
         highest_healpix_order=10,
-        dask_n_workers=1,
+        dask_n_workers=10,
         dask_threads_per_worker=1,
         dask_tmp="/data3/epyc/data3/hipscat/tmp/",
-        output_path="/data3/epyc/data3/hipscat/catalogs/",
+        output_path="/data3/epyc/data3/hipscat/test_catalogs/",
         completion_email_address="delucchi@andrew.cmu.edu",
         use_schema_file="/data3/epyc/data3/hipscat/tmp/tic_schema.parquet",
     )
