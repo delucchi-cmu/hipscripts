35a17ebc34ae9569e55006b77a65b7611190408f
Melissa DeLucchi <mmd11@epyc.astro.washington.edu>
2024-11-26 06:10:52 -0800

    checkpoint

--- epyc/hats_conversion/convert_ztf.py
+++ epyc/hats_conversion/convert_ztf.py
@@ -8,38 +8,38 @@ def convert_existing():
 
     with Client(
         local_directory="/data3/epyc/data3/hats/tmp/",
-        n_workers=20,
+        n_workers=35,
         threads_per_worker=1,
     ) as client:
-        subcatalog = "ztf_dr14"
-        print("starting subcatalog", subcatalog)
-        args = ConversionArguments(
-            input_catalog_path=f"/data3/epyc/data3/hipscat/catalogs/ztf_axs/{subcatalog}",
-            output_path="/data3/epyc/data3/hats/catalogs/ztf_dr14",
-            output_artifact_name=subcatalog,
-            completion_email_address="delucchi@andrew.cmu.edu",
-        )
-        runner.pipeline_with_client(args, client)
+        # subcatalog = "ztf_dr14"
+        # print("starting subcatalog", subcatalog)
+        # args = ConversionArguments(
+        #     input_catalog_path=f"/data3/epyc/data3/hipscat/catalogs/ztf_axs/{subcatalog}",
+        #     output_path="/data3/epyc/data3/hats/catalogs/ztf_dr14",
+        #     output_artifact_name=subcatalog,
+        #     completion_email_address="delucchi@andrew.cmu.edu",
+        # )
+        # runner.pipeline_with_client(args, client)
 
-        subcatalog = "ztf_dr14_10arcs"
-        print("starting subcatalog", subcatalog)
-        args = ConversionArguments(
-            input_catalog_path=f"/data3/epyc/data3/hipscat/catalogs/{subcatalog}",
-            output_path="/data3/epyc/data3/hats/catalogs/ztf_dr14",
-            output_artifact_name=subcatalog,
-            completion_email_address="delucchi@andrew.cmu.edu",
-        )
-        runner.pipeline_with_client(args, client)
+        # subcatalog = "ztf_dr14_10arcs"
+        # print("starting subcatalog", subcatalog)
+        # args = ConversionArguments(
+        #     input_catalog_path=f"/data3/epyc/data3/hipscat/catalogs/{subcatalog}",
+        #     output_path="/data3/epyc/data3/hats/catalogs/ztf_dr14",
+        #     output_artifact_name=subcatalog,
+        #     completion_email_address="delucchi@andrew.cmu.edu",
+        # )
+        # runner.pipeline_with_client(args, client)
 
-        subcatalog = "ztf_zource"
-        print("starting subcatalog", subcatalog)
-        args = ConversionArguments(
-            input_catalog_path=f"/data3/epyc/data3/hipscat/catalogs/ztf_axs/{subcatalog}",
-            output_path="/data3/epyc/data3/hats/catalogs/ztf_dr14",
-            output_artifact_name=subcatalog,
-            completion_email_address="delucchi@andrew.cmu.edu",
-        )
-        runner.pipeline_with_client(args, client)
+        # subcatalog = "ztf_source"
+        # print("starting subcatalog", subcatalog)
+        # args = ConversionArguments(
+        #     input_catalog_path=f"/data3/epyc/data3/hipscat/catalogs/ztf_axs/ztf_zource",
+        #     output_path="/data3/epyc/data3/hats/catalogs/ztf_dr14",
+        #     output_artifact_name=subcatalog,
+        #     completion_email_address="delucchi@andrew.cmu.edu",
+        # )
+        # runner.pipeline_with_client(args, client)
 
         subcatalog = "zubercal"
         print("starting subcatalog", subcatalog)
---
+++ epyc/margin/coarse_margins.py
@@ -0,0 +1,60 @@
+import hats_import.pipeline as runner
+from hats_import.margin_cache.margin_cache_arguments import MarginCacheArguments
+
+
+def make_gaia_margins():
+    args = MarginCacheArguments(
+        input_catalog_path="/data3/epyc/data3/hats/catalogs/gaia_dr3/gaia",
+        output_path="/data3/epyc/data3/hats/catalogs/coarse_margins",
+        output_artifact_name="gaia_10arcs",
+        margin_threshold=10,
+        dask_n_workers=30,
+        dask_threads_per_worker=1,
+        dask_tmp="/data3/epyc/data3/hats/tmp/",
+        completion_email_address="delucchi@andrew.cmu.edu",
+    )
+    runner.pipeline(args)
+
+def make_margins_distance():
+    args = MarginCacheArguments(
+        input_catalog_path="/data3/epyc/data3/hats/catalogs/gaia_dr3/gaia_edr3_distances",
+        output_path="/data3/epyc/data3/hats/catalogs/coarse_margins",
+        output_artifact_name="gaia_edr3_distances_10arcs",
+        margin_threshold=10,
+        dask_n_workers=30,
+        dask_threads_per_worker=1,
+        dask_tmp="/data3/epyc/data3/hats/tmp/",
+        completion_email_address="delucchi@andrew.cmu.edu",
+    )
+    runner.pipeline(args)
+
+def tic_margins():
+    args = MarginCacheArguments(
+        input_catalog_path="/data3/epyc/data3/hats/catalogs/tic/tic",
+        output_path="/data3/epyc/data3/hats/catalogs/coarse_margins",
+        output_artifact_name="tic_10arcs",
+        margin_threshold=10,
+        dask_n_workers=30,
+        dask_threads_per_worker=1,
+        dask_tmp="/data3/epyc/data3/hats/tmp/",
+        completion_email_address="delucchi@andrew.cmu.edu",
+    )
+    runner.pipeline(args)
+
+
+def ztf_dr14_margins():
+    args = MarginCacheArguments(
+        input_catalog_path="/data3/epyc/data3/hats/catalogs/ztf_dr14/ztf_dr14",
+        output_path="/data3/epyc/data3/hats/catalogs/coarse_margins",
+        output_artifact_name="ztf_dr14_10arcs",
+        margin_threshold=10,
+        dask_n_workers=30,
+        dask_threads_per_worker=1,
+        dask_tmp="/data3/epyc/data3/hats/tmp/",
+        completion_email_address="delucchi@andrew.cmu.edu",
+    )
+    runner.pipeline(args)
+
+
+if __name__ == "__main__":
+    make_margins_distance()
